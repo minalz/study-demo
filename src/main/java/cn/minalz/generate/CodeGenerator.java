bfd75f74ad7432d269aaa63ed59f0c7338fb8a23
minalz <119687281@qq.com>
2021-03-07 01:27:55 +0800

    1.添加mybatis-plus代码生成器 2.对beetl模版进行微改

--- src/main/java/cn/minalz/generate/CodeGenerator.java
+++ src/main/java/cn/minalz/generate/CodeGenerator.java
@@ -1,15 +1,8 @@
 package cn.minalz.generate;
 
 import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
-import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.BeetlTemplateEngine;
 
@@ -49,7 +42,7 @@ public class CodeGenerator {
 //        gc.setServiceName("%sService"); //去掉Service接口的首字母I
 //        gc.setIdType(IdType.ID_WORKER_STR); //主键策略
 //        gc.setDateType(DateType.ONLY_DATE);//定义生成的实体类中日期类型
-        gc.setSwagger2(true);//开启Swagger2模式
+        gc.setSwagger2(true); //开启Swagger2模式
 
         mpg.setGlobalConfig(gc);
 
@@ -75,11 +68,11 @@ public class CodeGenerator {
 
         // 5、策略配置
         StrategyConfig strategy = new StrategyConfig();
-        strategy.setInclude("user");//对哪一张表生成代码
-        strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
+        strategy.setInclude("user"); //对哪一张表生成代码
+        strategy.setNaming(NamingStrategy.underline_to_camel); //数据库表映射到实体的命名策略
         strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
 
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel); //数据库表字段映射到实体的命名策略
         strategy.setEntityLombokModel(true); // lombok 模型 @Accessors(chain = true) setter链式操作
 
         strategy.setRestControllerStyle(true); //restful api风格控制器
