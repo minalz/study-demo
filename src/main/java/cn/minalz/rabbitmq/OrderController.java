1e6bc7ebce03961ed1e93d069c488824a2d15857
minalz <119687281@qq.com>
2021-04-15 18:18:54 +0800

    添加RabbitMQ延时消息处理 利用的是死信交换机

---
+++ src/main/java/cn/minalz/rabbitmq/OrderController.java
@@ -0,0 +1,49 @@
+package cn.minalz.rabbitmq;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+ 
+/**
+ * @description: 订单的控制器
+ **/
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    private static final Logger logger =  LoggerFactory.getLogger(OrderController.class);
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    /**
+     * 模拟提交订单
+     * @author nxq
+     * @return java.lang.Object
+     */
+    @GetMapping("")
+    public Object submit(){
+        String orderId = UUID.randomUUID().toString();
+//        logger.info("submit order {}", orderId);
+        this.rabbitTemplate.convertAndSend(
+                RabbitMQConfiguration.orderExchange, //发送至订单交换机
+                RabbitMQConfiguration.routingKeyOrder, //订单定routingKey
+                orderId //订单号   这里可以传对象 比如直接传订单对象
+                , message -> {
+            // 如果配置了 params.put("x-message-ttl", 5 * 1000);
+            // 那么这一句也可以省略,具体根据业务需要是声明 Queue 的时候就指定好延迟时间还是在发送自己控制时间
+            message.getMessageProperties().setExpiration(1000 * 10 + "");
+            return message;
+        });
+        System.out.println("创建订单：orderId " + orderId + " .... " + sdf.format(new Date()));
+        return "{'orderId':'"+orderId+"'}";
+    }
+}
---
+++ src/main/java/cn/minalz/rabbitmq/OrderFailureListener.java
@@ -0,0 +1,41 @@
+package cn.minalz.rabbitmq;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.stereotype.Component;
+ 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import com.rabbitmq.client.Channel;
+ 
+/**
+ * @description: 订单失效监听器
+ **/
+@Component
+public class OrderFailureListener {
+    private static final Logger logger =  LoggerFactory.getLogger(OrderFailureListener.class);
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    @RabbitListener(
+            queues = RabbitMQConfiguration.dealQueueOrder //设置订单失效的队列
+    )
+    public void process(String order, Message message, @Headers Map<String, Object> headers, Channel channel) throws IOException {
+ 
+//        logger.info("【订单号】 - [{}]",  order);
+        // 判断订单是否已经支付，如果支付则；否则，取消订单（逻辑代码省略)
+ 
+        // 手动ack
+//        Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        // 手动签收
+//        channel.basicAck(deliveryTag, false);
+        System.out.println("执行结束：order + " + order + " .... " + sdf.format(new Date()));
+ 
+    }
+}
