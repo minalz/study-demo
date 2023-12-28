1069e73a6428923592ca9ec08451797b476cd1dc
zhouwei <zhouwei@hywintech.com>
2023-12-28 10:14:41 +0800

    feat: update 并且配置文件中的ip 配置都失效了

--- src/main/java/cn/minalz/KernelApplication.java
+++ src/main/java/cn/minalz/KernelApplication.java
@@ -3,10 +3,12 @@ package cn.minalz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 /**
  * 启动类
  */
+@EnableRetry
 @SpringBootApplication
 @MapperScan("cn.minalz.mapper")
 public class KernelApplication {
